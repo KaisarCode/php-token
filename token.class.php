05640f694ad728748501cf3e54c61adcc0d76dfc
KaisarCode <kaisar@kaisarcode.com>
2021-01-03 01:22:30 -0300

    Add functions file

---
+++ token.class.php
@@ -0,0 +1,34 @@
+<?php
+namespace KC;
+
+Class Token {
+    
+    // Generate key
+    static function generateKey($length = 8) {
+        $pool = array_merge(range(0,9),
+        range('a', 'z'), range('A', 'Z'));
+        $key = '';
+        for($i=0; $i < $length; $i++) {
+            $key .= $pool[mt_rand(0, count($pool) - 1)];
+        }
+        return $key;
+    }
+    
+    // Generate token
+    static function generateToken() {
+        if (version_compare(PHP_VERSION, '7.0.0') >= 0) {
+            $token = bin2hex(random_bytes(16));
+        } else {
+            $token = openssl_random_pseudo_bytes(16);
+            $token = bin2hex($token);
+        }
+        return $token;
+    }
+    
+    // Generate expiration duration
+    static function generateExpiration($s = 1) {
+        if (is_nan($s)) $s = 0;
+        $ts = time();
+        return round($ts / $s);
+    }
+}
