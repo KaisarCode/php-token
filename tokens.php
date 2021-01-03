007e8133fed6dfcb9fa665f6eb581fb013d6f351
KaisarCode <kaisar@kaisarcode.com>
2021-01-03 00:28:35 -0300

    Make class

--- tokens.php
+++ tokens.php
@@ -1,30 +1,34 @@
 <?php
+namespace KC;
 
-// Generate key
-function generateKey($length = 8) {
-    $pool = array_merge(range(0,9),
-    range('a', 'z'), range('A', 'Z'));
-    $key = '';
-    for($i=0; $i < $length; $i++) {
-        $key .= $pool[mt_rand(0, count($pool) - 1)];
+Class Token {
+    
+    // Generate key
+    static function generateKey($length = 8) {
+        $pool = array_merge(range(0,9),
+        range('a', 'z'), range('A', 'Z'));
+        $key = '';
+        for($i=0; $i < $length; $i++) {
+            $key .= $pool[mt_rand(0, count($pool) - 1)];
+        }
+        return $key;
     }
-    return $key;
-}
-
-// Generate token
-function generateToken() {
-    if (version_compare(PHP_VERSION, '7.0.0') >= 0) {
-        $token = bin2hex(random_bytes(16));
-    } else {
-        $token = openssl_random_pseudo_bytes(16);
-        $token = bin2hex($token);
+    
+    // Generate token
+    static function generateToken() {
+        if (version_compare(PHP_VERSION, '7.0.0') >= 0) {
+            $token = bin2hex(random_bytes(16));
+        } else {
+            $token = openssl_random_pseudo_bytes(16);
+            $token = bin2hex($token);
+        }
+        return $token;
     }
-    return $token;
-}
-
-// Generate expiration duration
-function generateExpiration($s = 1) {
-    if (is_nan($s)) $s = 0;
-    $ts = time();
-    return round($ts / $s);
+    
+    // Generate expiration duration
+    static function generateExpiration($s = 1) {
+        if (is_nan($s)) $s = 0;
+        $ts = time();
+        return round($ts / $s);
+    }
 }
